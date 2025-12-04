70eac07854fa04250709ca29f7430cf1204a5d2d
Nakshatra Namaha Creations <support@vijayhomeservices.com>
2025-12-04 13:17:35 +0530

    NEW CHANFGES

--- Configuration/firebaseAdmin.js
+++
@@ -1,8 +0,0 @@
-const admin = require("firebase-admin");
-const serviceAccount = require("./adminkey.json"); // Path to your Firebase Admin SDK JSON file
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
-module.exports = admin;
--- Firebase/index.js
+++
@@ -1,8 +0,0 @@
-const firebase = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
-
-firebase.initializeApp({
-  credential: firebase.credential.cert(serviceAccount),
-});
-
-module.exports = { firebase };
